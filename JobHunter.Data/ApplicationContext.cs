cadcbddced169e4faaeb759115c21c7a91c54dd6
VitaliiYalovenko <nerevit7@gmail.com>
2019-02-11 16:00:15 +0200

    create skeleton

---
+++ JobHunter.Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using JobHunter.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobHunter.Data
+{
+    public class ApplicationContext : IdentityDbContext<User, IdentityRole<int>,int>
+    {
+
+    }
+}
