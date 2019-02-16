712f9a2fe87c98e85cb9701a69a4f93b87c944b9
ValentynYatsiuk <iatsiuk_ak16@nuwm.edu.ua>
2019-02-17 00:33:07 +0200

    try_to_solve_issues

--- JobHunter.Presentation/ClientApp/src/app/app.module.ts
+++ JobHunter.Presentation/ClientApp/src/app/app.module.ts
@@ -7,8 +7,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
-import { CounterComponent } from './components/counter/counter.component';
-import { FetchDataComponent } from './components/fetch-data/fetch-data.component';
+
 import {RegistrationComponent} from './components/registration/registration.component';
 
 @NgModule({
@@ -16,8 +15,7 @@ import {RegistrationComponent} from './components/registration/registration.comp
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
+   
     RegistrationComponent,
   ],
   imports: [
@@ -26,8 +24,7 @@ import {RegistrationComponent} from './components/registration/registration.comp
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+     
       { path: 'registration', component: RegistrationComponent },
     ])
   ],
